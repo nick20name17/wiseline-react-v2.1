0d304e57b4425780c1c6c5675912581ad6dab3c5
Nazar <nick20name17@gmail.com>
2024-11-29 11:01:32 +0200

    init

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./components/ui/button"
+
+export const App = () => {
+  return (
+    <div className="h-screen flex items-center justify-center">
+        <Button>App</Button>
+    </div>
+  )
+}
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    plugins: [
+        react({
+            babel: {
+                plugins: [['babel-plugin-react-compiler', {}]]
+            }
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src')
+        }
+    }
+})
