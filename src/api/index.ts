59910caf3d95171408e9d77e8b361fc4a7a6a725
Nazar <nick20name17@gmail.com>
2024-11-29 16:03:25 +0200

    fixes

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,7 +5,6 @@ import {
     createApi,
     fetchBaseQuery
 } from '@reduxjs/toolkit/query/react'
-import type { RootState } from '..'
 
 import type {
     AccessToken,
@@ -15,6 +14,7 @@ import type {
     RefreshToken
 } from '@/api/types/auth'
 import { apiBaseUrl } from '@/config/app'
+import type { RootState } from '@/store'
 import { logout, tokenUpdated } from '@/store/slices/auth'
 import type { User } from './users/users.types'
 
