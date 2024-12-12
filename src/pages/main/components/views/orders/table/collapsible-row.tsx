5a2ff851c03e218223d8edb6239469dc30d38dcc
Nazar <nick20name17@gmail.com>
2024-12-12 13:53:01 +0200

    fixes

--- src/pages/main/components/views/orders/table/collapsible-row.tsx
+++ src/pages/main/components/views/orders/table/collapsible-row.tsx
@@ -1,10 +1,13 @@
 import { type Row, flexRender } from '@tanstack/react-table'
-import { useState } from 'react'
+import { format } from 'date-fns'
+import { Fragment, useState } from 'react'
 import { StringParam, useQueryParam } from 'use-query-params'
 
 import { SubTable } from '../sub-table/sub-table'
 
+import { columns } from './columns'
 import type { OrdersData } from '@/api/ebms/ebms.types'
+import { Checkbox } from '@/components/ui/checkbox'
 import { Collapsible, CollapsibleContent } from '@/components/ui/collapsible'
 import { TableCell, TableRow } from '@/components/ui/table'
 import { shouldRenderCell } from '@/config/table'
@@ -87,66 +90,65 @@ export const CollapsibleRow = ({ row }: CollapsibleRowProps) => {
     )
 }
 
-// interface GroupedRowsProps {
-//     originRow: Row<OrdersData>
-//     groupByDate: [string, Row<OrdersData>[]][]
-// }
+interface GroupedRowsProps {
+    groupByDate: [string, Row<OrdersData>[]][]
+}
 
-// export const CollapsibleGroupedRows = ({ groupByDate }: GroupedRowsProps) => {
-//     const columnsCount = columns.length
+export const CollapsibleGroupedRows = ({ groupByDate }: GroupedRowsProps) => {
+    const columnsCount = columns.length
 
-//     return groupByDate.map((group) =>
-//         group[1].map((row, index) => {
-//             const isIndeterminate =
-//                 group[1].some((row) => row.getIsSelected()) &&
-//                 !group[1].every((row) => row.getIsSelected())
-//                     ? 'indeterminate'
-//                     : group[1].every((row) => row.getIsSelected())
+    return groupByDate.map((group) =>
+        group[1].map((row, index) => {
+            const isIndeterminate =
+                group[1].some((row) => row.getIsSelected()) &&
+                !group[1].every((row) => row.getIsSelected())
+                    ? 'indeterminate'
+                    : group[1].every((row) => row.getIsSelected())
 
-//             return (
-//                 <Fragment key={group[1][0].original.id}>
-//                     {index === 0 && (
-//                         <TableRow
-//                             id={'tr-group=header-' + row.original?.id}
-//                             className='bg-secondary !p-0'
-//                         >
-//                             <TableCell
-//                                 colSpan={columnsCount}
-//                                 className='!p-0'
-//                             >
-//                                 <div className='!m-0 flex h-9 items-center bg-[#E6E6E6]'>
-//                                     <div className='sticky left-0 z-50 flex h-full w-10 items-center justify-center bg-[#E6E6E6]'>
-//                                         <Checkbox
-//                                             checked={isIndeterminate}
-//                                             value={row.id}
-//                                             onCheckedChange={(value) => {
-//                                                 group[1].forEach((row) =>
-//                                                     row.toggleSelected(!!value)
-//                                                 )
-//                                             }}
-//                                             aria-label='Select row'
-//                                         />
-//                                     </div>
-//                                     <div className='pl-4'>
-//                                         {group[1][0].original?.sales_order
-//                                             ?.production_date
-//                                             ? format(
-//                                                   group[1][0].original?.sales_order
-//                                                       ?.production_date || '',
-//                                                   'dd.MM.yyyy EEE'
-//                                               )
-//                                             : '-'}
-//                                     </div>
-//                                 </div>
-//                             </TableCell>
-//                         </TableRow>
-//                     )}
-//                     <CollapsibleRow row={group[1][0]} />
-//                 </Fragment>
-//             )
-//         })
-//     )
-// }
+            return (
+                <Fragment key={group[1][0].original.id}>
+                    {index === 0 && (
+                        <TableRow
+                            id={'tr-group=header-' + row.original?.id}
+                            className='bg-secondary !p-0'
+                        >
+                            <TableCell
+                                colSpan={columnsCount}
+                                className='!p-0'
+                            >
+                                <div className='!m-0 flex h-8 items-center !bg-gray-300 text-sm'>
+                                    <div className='sticky left-0 flex h-full w-10 items-center justify-center !bg-gray-300 shadow-[inset_-1px_0_0] shadow-border'>
+                                        <Checkbox
+                                            checked={isIndeterminate}
+                                            value={row.id}
+                                            onCheckedChange={(value) => {
+                                                group[1].forEach((row) =>
+                                                    row.toggleSelected(!!value)
+                                                )
+                                            }}
+                                            aria-label='Select row'
+                                        />
+                                    </div>
+                                    <div className='pl-4'>
+                                        {group[1][0].original?.sales_order
+                                            ?.production_date
+                                            ? format(
+                                                  group[1][0].original?.sales_order
+                                                      ?.production_date || '',
+                                                  'dd.MM.yyyy EEE'
+                                              )
+                                            : '-'}
+                                    </div>
+                                </div>
+                            </TableCell>
+                        </TableRow>
+                    )}
+                    <CollapsibleRow row={group[1][0]} />
+                </Fragment>
+            )
+        })
+    )
+}
 
 interface CollapsibleRowProps {
     row: Row<OrdersData>
--- src/pages/main/components/views/orders/table/table.tsx
+++ src/pages/main/components/views/orders/table/table.tsx
@@ -4,10 +4,10 @@ import {
     getSortedRowModel,
     useReactTable
 } from '@tanstack/react-table'
-import { useRef } from 'react'
+import { memo, useCallback, useMemo, useRef } from 'react'
 import { BooleanParam, StringParam, useQueryParam } from 'use-query-params'
 
-import { CollapsibleRow } from './collapsible-row'
+import { CollapsibleGroupedRows, CollapsibleRow } from './collapsible-row'
 import { TableFooter } from './table-footer'
 import type { OrdersData } from '@/api/ebms/ebms.types'
 import {
@@ -37,170 +37,201 @@ import { useSorting } from '@/hooks/use-sorting'
 import { useTableScroll } from '@/hooks/use-table-scroll'
 import { cn } from '@/lib/utils'
 import type { DataTableProps } from '@/types/table'
+import { groupByDate } from '@/utils/group-by'
 
-export const OrdersViewTable = ({
-    columns,
-    data,
-    isDataLoading,
-    isDataFetching,
-    pageCount
-}: DataTableProps<OrdersData, OrdersData>) => {
-    const [category] = useQueryParam('category', StringParam)
-    const [view] = useQueryParam('view', StringParam)
-    const [completed] = useQueryParam('completed', BooleanParam)
-
-    const { data: usersProfilesData } = useGetUsersProfilesQuery()
-    const [addUsersProfiles] = useAddUsersProfilesMutation()
-
-    const { columnOrder } = useColumnOrder(usersProfilesData!, 'orders')
-
-    const { columnVisibility } = useColumnVisibility(
-        usersProfilesData!,
-        'orders',
-        columns
-    )
-
-    const { sorting, setSorting } = useSorting([
-        {
-            id: completed ? 'priority' : 'priority',
-            desc: true
-        }
-    ])
-    const table = useReactTable({
-        getCoreRowModel: getCoreRowModel(),
-        getSortedRowModel: getSortedRowModel(),
-        onSortingChange: setSorting,
-        data,
+const OrdersViewTable = memo(
+    ({
         columns,
-        manualPagination: true,
-        manualSorting: true,
-        pageCount,
-        state: {
-            columnVisibility,
-            sorting,
-            columnOrder,
-            columnPinning: {
-                left: ['select', 'arrow']
+        data,
+        isDataLoading,
+        isDataFetching,
+        pageCount
+    }: DataTableProps<OrdersData, OrdersData>) => {
+        const [category] = useQueryParam('category', StringParam)
+        const [view] = useQueryParam('view', StringParam)
+        const [completed] = useQueryParam('completed', BooleanParam)
+
+        const { data: usersProfilesData } = useGetUsersProfilesQuery()
+        const [addUsersProfiles] = useAddUsersProfilesMutation()
+
+        const { columnOrder } = useColumnOrder(usersProfilesData!, 'orders')
+        const { columnVisibility } = useColumnVisibility(
+            usersProfilesData!,
+            'orders',
+            columns
+        )
+        const { sorting, setSorting } = useSorting([
+            { id: completed ? 'priority' : 'production_date', desc: true }
+        ])
+
+        const table = useReactTable({
+            getCoreRowModel: getCoreRowModel(),
+            getSortedRowModel: getSortedRowModel(),
+            onSortingChange: setSorting,
+            data,
+            columns,
+            manualPagination: true,
+            manualSorting: true,
+            pageCount,
+            state: {
+                columnVisibility,
+                sorting,
+                columnOrder,
+                columnPinning: { left: ['select', 'arrow'] }
             }
-        }
-    })
+        })
 
-    const { onDragStart, onDrop } = useColumnDragDrop(table, 'orders', addUsersProfiles)
+        const { onDragStart, onDrop } = useColumnDragDrop(
+            table,
+            'orders',
+            addUsersProfiles
+        )
 
-    const tableRef = useRef<HTMLTableElement>(null)
+        const tableRef = useRef<HTMLTableElement>(null)
+        const { isTablet } = useMatchMedia()
 
-    const { isTablet } = useMatchMedia()
+        useTableScroll({
+            tableRef,
+            enableScroll: !isTablet,
+            isCuttingView: view === 'cut'
+        })
 
-    useTableScroll({
-        tableRef,
-        enableScroll: !isTablet,
-        isCuttingView: view === 'cut'
-    })
+        const isClientOrWorker = useCurrentUserRole(['client', 'worker'])
 
-    const isClientOrWorker = useCurrentUserRole(['client', 'worker'])
+        const groupData = useMemo(
+            () =>
+                groupByDate(
+                    table?.getRowModel()?.rows || [],
+                    (row) => row.original?.sales_order?.production_date || ''
+                ),
+            [table?.getRowModel()?.rows]
+        )
 
-    return (
-        <>
-            <ScrollArea
-                ref={tableRef}
-                className={cn('mt-4 rounded-md border')}
-            >
-                <Table>
-                    <TableHeader className='sticky top-0 z-40 bg-background bg-gray-100'>
-                        {isDataLoading ? (
-                            <TableRow>
-                                <TableCell colSpan={columns.length}>
-                                    <Skeleton className='h-5 w-full' />
-                                </TableCell>
-                            </TableRow>
-                        ) : (
-                            table.getHeaderGroups().map((headerGroup) => (
-                                <TableRow key={headerGroup.id}>
-                                    {headerGroup.headers.map((header, i) => {
-                                        return shouldRenderCell(
-                                            header.column.id,
-                                            category!,
-                                            isClientOrWorker,
-                                            i
-                                        ) ? (
-                                            <TableHead
-                                                className={cn(
-                                                    header.column.getIsPinned()
-                                                        ? 'sticky left-0 border-b-0 border-r-0 bg-secondary shadow-[inset_-1px_-1px_0] shadow-border'
-                                                        : '',
-                                                    header.column.id === 'arrow'
-                                                        ? 'left-10'
-                                                        : ''
-                                                )}
-                                                style={{
-                                                    maxWidth:
-                                                        header.column.columnDef.size,
-                                                    minWidth: header.column.columnDef.size
-                                                }}
-                                                key={header.id}
-                                                colSpan={header.colSpan}
-                                                draggable={
-                                                    !table.getState().columnSizingInfo
-                                                        .isResizingColumn
-                                                }
-                                                data-column-index={header.index}
-                                                onDragStart={onDragStart}
-                                                onDragOver={(e) => {
-                                                    e.stopPropagation()
-                                                    e.preventDefault()
-                                                }}
-                                                onDrop={onDrop}
-                                            >
-                                                {header.isPlaceholder
-                                                    ? null
-                                                    : flexRender(
-                                                          header.column.columnDef.header,
-                                                          header.getContext()
-                                                      )}
-                                            </TableHead>
-                                        ) : null
-                                    })}
-                                </TableRow>
-                            ))
-                        )}
-                    </TableHeader>
-                    <TableBody className='[&_tr:last-child]:!border-b-0'>
-                        {/* <AnimatePresence initial={false}> */}
-                        {isDataLoading ? (
-                            <TableSkeleton columnsCount={columns.length} />
-                        ) : table.getRowModel().rows?.length ? (
-                            table.getRowModel().rows.map((row) => {
-                                return (
-                                    <CollapsibleRow
-                                        key={row.original.id}
-                                        row={row}
-                                    />
-                                )
-                            })
-                        ) : isDataFetching ? (
-                            <TableSkeleton columnsCount={columns.length} />
-                        ) : (
-                            <TableRow className='!bg-transparent'>
-                                <TableCell
-                                    colSpan={columns.length}
-                                    className='h-24 !bg-transparent pl-10 pt-9 text-left'
-                                >
-                                    No results.
-                                </TableCell>
-                            </TableRow>
-                        )}
-                        {/* </AnimatePresence> */}
-                    </TableBody>
-                </Table>
-                <ScrollBar orientation='horizontal' />
-            </ScrollArea>
+        const renderHeader = useCallback(() => {
+            if (isDataLoading) {
+                return (
+                    <TableRow>
+                        <TableCell colSpan={columns.length}>
+                            <Skeleton className='h-5 w-full' />
+                        </TableCell>
+                    </TableRow>
+                )
+            }
 
-            <TableFooter
-                isDataFetching={isDataFetching}
-                isDataLoading={isDataLoading}
-                pageCount={pageCount}
-                table={table}
-            />
-        </>
-    )
-}
+            return table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                    {headerGroup.headers.map((header, i) =>
+                        shouldRenderCell(
+                            header.column.id,
+                            category!,
+                            isClientOrWorker,
+                            i
+                        ) ? (
+                            <TableHead
+                                key={header.id}
+                                className={cn(
+                                    header.column.getIsPinned()
+                                        ? 'sticky left-0 border-b-0 border-r-0 bg-secondary shadow-[inset_-1px_-1px_0] shadow-border'
+                                        : '',
+                                    header.column.id === 'arrow' ? 'left-10' : ''
+                                )}
+                                style={{
+                                    maxWidth: header.column.columnDef.size,
+                                    minWidth: header.column.columnDef.size
+                                }}
+                                colSpan={header.colSpan}
+                                draggable={
+                                    !table.getState().columnSizingInfo.isResizingColumn
+                                }
+                                data-column-index={header.index}
+                                onDragStart={onDragStart}
+                                onDragOver={(e) => {
+                                    e.stopPropagation()
+                                    e.preventDefault()
+                                }}
+                                onDrop={onDrop}
+                            >
+                                {header.isPlaceholder
+                                    ? null
+                                    : flexRender(
+                                          header.column.columnDef.header,
+                                          header.getContext()
+                                      )}
+                            </TableHead>
+                        ) : null
+                    )}
+                </TableRow>
+            ))
+        }, [
+            isDataLoading,
+            table,
+            category,
+            isClientOrWorker,
+            columns.length,
+            onDragStart,
+            onDrop
+        ])
+
+        const renderBody = useCallback(() => {
+            if (isDataLoading) {
+                return <TableSkeleton columnsCount={columns.length} />
+            }
+
+            if (table.getRowModel().rows?.length) {
+                return completed ? (
+                    <CollapsibleGroupedRows groupByDate={groupData} />
+                ) : (
+                    table.getRowModel().rows.map((row) => (
+                        <CollapsibleRow
+                            key={row.original.id}
+                            row={row}
+                        />
+                    ))
+                )
+            }
+
+            if (isDataFetching) {
+                return <TableSkeleton columnsCount={columns.length} />
+            }
+
+            return (
+                <TableRow className='!bg-transparent'>
+                    <TableCell
+                        colSpan={columns.length}
+                        className='h-24 !bg-transparent pl-10 pt-9 text-left'
+                    >
+                        Немає результатів.
+                    </TableCell>
+                </TableRow>
+            )
+        }, [isDataLoading, isDataFetching, table, completed, groupData, columns])
+
+        return (
+            <>
+                <ScrollArea
+                    ref={tableRef}
+                    className={cn('mt-4 rounded-md border')}
+                >
+                    <Table>
+                        <TableHeader className='sticky top-0 z-40 bg-background bg-gray-100'>
+                            {renderHeader()}
+                        </TableHeader>
+                        <TableBody className='[&_tr:last-child]:!border-b-0'>
+                            {renderBody()}
+                        </TableBody>
+                    </Table>
+                    <ScrollBar orientation='horizontal' />
+                </ScrollArea>
+
+                <TableFooter
+                    isDataFetching={isDataFetching}
+                    isDataLoading={isDataLoading}
+                    pageCount={pageCount}
+                    table={table}
+                />
+            </>
+        )
+    }
+)
+
+export { OrdersViewTable }
